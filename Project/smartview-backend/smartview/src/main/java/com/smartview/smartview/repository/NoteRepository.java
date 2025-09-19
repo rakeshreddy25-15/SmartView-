9dda4972bdc24902154f7e2b5e74cbb0e7d2d65e
rakeshreddy25-15 <rakeshreddy251507@gmail.com>
2025-09-19 21:52:06 +0530

    Delete Project/smartview-backend/smartview directory

--- Project/smartview-backend/smartview/src/main/java/com/smartview/smartview/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartview.smartview.repository;
-
-import com.smartview.smartview.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findByUserEmail(String userEmail);
-}
